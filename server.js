1fc97d0c6eb74dc6f3619b3e13e8cf455664c685
Colin MacDonald <colin@goinstant.com>
2014-09-28 23:37:47 -0300

    use env json config

--- server.js
+++ server.js
@@ -5,12 +5,12 @@
 var http = require('http');
 
 var express = require('express');
-var helmet = require('helmet');
-var passport = require('passport');
+var config = require('env-json');
 
 var configure = require('./lib/config/configure');
 
-var PORT = process.env.PORT || 3000;
+config.initialize('basicAuthTemplate');
+config.set('port', process.env.PORT || 3000);
 
 var app = express();
 
@@ -18,9 +18,9 @@ configure.all(app);
 
 var server = http.createServer(app);
 
-server.listen(PORT, function(err) {
+server.listen(config.port, function(err) {
   if (err) {
     throw err;
   }
-  console.log('Basic Auth Server listening on port:', PORT);
+  console.log('Basic Auth Server listening on port:', config.port);
 });
